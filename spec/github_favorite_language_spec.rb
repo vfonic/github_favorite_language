74f602dfca45ad6d1ecabedbc4616bcb24888a75
Viktor Fonic <viktor.fonic@gmail.com>
2015-06-26 23:13:32 +0100

    Get to the green

--- spec/github_favorite_language_spec.rb
+++ spec/github_favorite_language_spec.rb
@@ -5,7 +5,10 @@ describe GithubFavoriteLanguage do
     expect(GithubFavoriteLanguage::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe ".get_favorite_language" do
+    it 'should print usage with whitespace username' do
+      expect(GithubFavoriteLanguage).to receive(:print_usage)
+      GithubFavoriteLanguage.get_favorite_language(username:'  ')
+    end
   end
 end
